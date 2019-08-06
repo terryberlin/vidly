0479ac86468d1deb4a479b26682c212930ef03d4
Terry Berlin <terryb@quikserve.com>
2019-08-06 16:18:05 -0500

    Chapter 24

---
+++ src/components/common/table.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+
+const Table = props => {
+  const { columns, sortColumn, onSort, data } = props;
+  return (
+    <table className="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody columns={columns} data={data} />
+    </table>
+  );
+};
+
+export default Table;
+
+{
+  /* <tbody>
+      {movies.map(movie => (
+        <tr key={movie._id}>
+          <td>{movie.title}</td>
+          <td>{movie.genre.name}</td>
+          <td>{movie.numberInStock}</td>
+          <td>{movie.dailyRentalRate}</td>
+          <td>
+            <Like liked={movie.liked} onClick={() => onLike(movie)} />
+          </td>
+          <td>
+            <button
+              onClick={() => onDelete(movie)}
+              className="btn btn-danger btn-sm"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      ))}
+    </tbody> */
+}
