32b7454005ebec917ae17bdf158810a23113bfda
Terry Berlin <terryb@quikserve.com>
2019-08-06 15:42:31 -0500

    Chapter 21 1st half

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
@@ -22,7 +23,9 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
-        <tbody>
+        <TableBody columns={this.columns} data={movies} />
+
+        {/* <tbody>
           {movies.map(movie => (
             <tr key={movie._id}>
               <td>{movie.title}</td>
@@ -42,7 +45,7 @@ class MoviesTable extends Component {
               </td>
             </tr>
           ))}
-        </tbody>
+        </tbody> */}
       </table>
     );
   }
